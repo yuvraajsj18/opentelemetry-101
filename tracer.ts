b78763c153e1ab85edeaf9b4c5aa5a1737a5388b
Yuvraj <yuvraajsj18@gmail.com>
2024-04-26 16:07:40 +0530

    added otel collector

--- tracer.ts
+++ tracer.ts
@@ -2,7 +2,8 @@ import { NodeSDK } from '@opentelemetry/sdk-node';
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
 import { PrometheusExporter } from '@opentelemetry/exporter-prometheus';
-import { MeterProvider } from '@opentelemetry/sdk-metrics';
+import { MeterProvider, PeriodicExportingMetricReader } from '@opentelemetry/sdk-metrics';
+import { OTLPMetricExporter} from '@opentelemetry/exporter-metrics-otlp-proto';
 import { Resource } from '@opentelemetry/resources';
 import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
 
@@ -10,23 +11,29 @@ import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions'
 function start(serviceName: string) {
 
     // Prometheus
-    const { endpoint, port } = PrometheusExporter.DEFAULT_OPTIONS;
-    const exporter = new PrometheusExporter({}, () => {
-        console.log(
-          `prometheus scrape endpoint: http://localhost:${port}${endpoint}`,
-        );
-      });
+    const { endpoint, port } = PrometheusExporter.DEFAULT_OPTIONS; 
+    // const exporter = new PrometheusExporter({}, () => {
+    //     console.log(
+    //       `prometheus scrape endpoint: http://localhost:${port}${endpoint}`,
+    //     );
+    //   });
     const meterProvider = new MeterProvider({
             resource: new Resource({
             [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
         }),
-    });    meterProvider.addMetricReader(exporter);
+    });    
+    const metricReader = new PeriodicExportingMetricReader({
+        exporter: new OTLPMetricExporter({
+            url:'http://collector:4318/v1/metrics'
+        })
+    })
+    meterProvider.addMetricReader(metricReader);
     const meter = meterProvider.getMeter('my-service-meter');
 
 
     // Jaeger
     const traceExporter = new OTLPTraceExporter({
-        url: 'http://jaeger:4318/v1/traces',
+        url: 'http://collector:4318/v1/traces',
     });
 
     const sdk = new NodeSDK({
